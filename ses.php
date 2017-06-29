e446604be0820365b39bb69e126cb3a759f77989
Alek-ON <gekko-7@mail.ru>
2017-06-29 11:41:16 +0300

    create project

---
+++ ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '889710a6028c548707c78e2e4f8bfa7f' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'en',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => '3306',
+      'my_user' => 's',
+      'my_pass' => '1e_D2pL94tpNM',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1498682799,
+    'lng' => 'en',
+  ),
+);
+?>
